b176a8053aef9441349a15056cd0031e53046ec2
Eric Ranschau <eranschau@horizontal.com>
2023-04-17 12:23:36 -0500

    NOJIR: Text component auto-documentation.

--- src/components/authorable/General/Text/Text.mock-data.ts
+++ src/components/authorable/General/Text/Text.mock-data.ts
@@ -4,3 +4,39 @@ const defaultData = {
 };
 
 export default defaultData;
+
+export const h1Data = {
+  className: 'text-3xl',
+  tag: 'h1',
+  text: 'h1. Heading',
+};
+
+export const h2Data = {
+  className: 'text-2xl',
+  tag: 'h2',
+  text: 'h2. Heading',
+};
+
+export const h3Data = {
+  className: 'text-xl',
+  tag: 'h3',
+  text: 'h3. Heading',
+};
+
+export const h4Data = {
+  className: 'text-lg',
+  tag: 'h4',
+  text: 'h4. Heading',
+};
+
+export const h5Data = {
+  className: 'text-md',
+  tag: 'h5',
+  text: 'h5. Heading',
+};
+
+export const h6Data = {
+  className: 'text-sm',
+  tag: 'h6',
+  text: 'h6. Heading',
+};
--- src/components/authorable/General/Text/Text.stories.tsx
+++ src/components/authorable/General/Text/Text.stories.tsx
@@ -3,17 +3,18 @@ import type { Meta, StoryObj } from '@storybook/react';
 
 // Local
 import Text, { TEXT_TAGS } from './Text';
-import defaultData from './Text.mock-data';
+import defaultData, { h1Data, h2Data, h3Data, h4Data, h5Data, h6Data } from './Text.mock-data';
 
 const meta: Meta<typeof Text> = {
-  title: 'Authorable/General/Text',
-  component: Text,
   argTypes: {
     tag: {
       control: 'select',
       options: TEXT_TAGS,
     },
   },
+  component: Text,
+  tags: ['autodocs'],
+  title: 'Authorable/General/Text',
 };
 
 export default meta;
@@ -23,5 +24,41 @@ type Story = StoryObj<typeof Text>;
 export const Default: Story = {
   args: {
     ...defaultData,
+  },
+};
+
+export const heading1: Story = {
+  args: {
+    ...h1Data,
+  },
+};
+
+export const heading2: Story = {
+  args: {
+    ...h2Data,
+  },
+};
+
+export const heading3: Story = {
+  args: {
+    ...h3Data,
+  },
+};
+
+export const heading4: Story = {
+  args: {
+    ...h4Data,
+  },
+};
+
+export const heading5: Story = {
+  args: {
+    ...h5Data,
+  },
+};
+
+export const heading6: Story = {
+  args: {
+    ...h6Data,
   },
 };
